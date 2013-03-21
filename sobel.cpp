ae73a7609b7c486cdfacc3766a6babc5569eb574
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2013-03-21 23:11:18 +0100

    initial project version

---
+++ sobel.cpp
@@ -0,0 +1,123 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <iostream>
+
+using namespace cv;
+
+std::string getImgType(int imgTypeInt);
+
+/** @function main */
+int main( int argc, char** argv )
+{
+
+  Mat src, src_gray;
+  Mat grad;
+  const char* window_name = "Sobel Demo - Simple Edge Detector";
+  int scale = 1;
+  int delta = 0;
+  int ddepth = CV_16S;
+
+  int c;
+
+  /// Load an image
+  src = imread( argv[1] );
+
+  if( !src.data )
+  { return -1; }
+
+  //GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
+
+  /// Convert it to gray
+  cvtColor( src, src_gray, CV_RGB2GRAY );
+
+  /// Create window
+  namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+
+  /// Generate grad_x and grad_y
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
+
+  /// Gradient X
+  //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
+  Sobel( src_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_x, abs_grad_x );
+
+  /// Gradient Y
+  //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
+  Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( grad_y, abs_grad_y );
+
+  /// Total Gradient (approximate)
+  addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
+
+
+  imshow( window_name, grad );
+
+  std::cout << "x type: " << getImgType(grad_x.type()) << "\t" << CV_16S << "\n";
+  std::cout << CV_32F << "\t" << CV_64F << "\n";
+
+  cv::Mat grad_x_f, grad_y_f;
+  grad_x.convertTo(grad_x_f,CV_32F);
+  grad_y.convertTo(grad_y_f,CV_32F);
+
+
+  cv::Mat mag;
+  cv::magnitude(grad_x_f,grad_y_f,mag);
+
+  cv::convertScaleAbs(mag,mag);
+  imshow("better gradient magnitude",mag);
+
+  cv::Mat mag_bin;
+  cv:threshold(mag,mag_bin,128,256,THRESH_BINARY);
+  imshow("bin better gradient magnitude",mag_bin);
+
+  cv::Mat ph;
+  cv::phase(grad_x_f,grad_y_f,ph,true);
+
+  cv::convertScaleAbs(ph,ph);
+  imshow("phase",ph);  
+
+  cv::Mat ph_interesting;
+  cv::bitwise_and(ph,mag_bin,ph_interesting);
+  imshow("interesting phase",ph_interesting);
+
+  cv::convertScaleAbs(grad_x,grad_x);
+  cv::convertScaleAbs(grad_y,grad_y);
+  imshow("x",grad_x);
+  imshow("y",grad_y);
+
+  waitKey(0);
+
+  return 0;
+  }
+
+
+  std::string getImgType(int imgTypeInt)
+{
+    int numImgTypes = 35; // 7 base types, with five channel options each (none or C1, ..., C4)
+
+    int enum_ints[] =       {CV_8U,  CV_8UC1,  CV_8UC2,  CV_8UC3,  CV_8UC4,
+                             CV_8S,  CV_8SC1,  CV_8SC2,  CV_8SC3,  CV_8SC4,
+                             CV_16U, CV_16UC1, CV_16UC2, CV_16UC3, CV_16UC4,
+                             CV_16S, CV_16SC1, CV_16SC2, CV_16SC3, CV_16SC4,
+                             CV_32S, CV_32SC1, CV_32SC2, CV_32SC3, CV_32SC4,
+                             CV_32F, CV_32FC1, CV_32FC2, CV_32FC3, CV_32FC4,
+                             CV_64F, CV_64FC1, CV_64FC2, CV_64FC3, CV_64FC4};
+
+    string enum_strings[] = {"CV_8U",  "CV_8UC1",  "CV_8UC2",  "CV_8UC3",  "CV_8UC4",
+                             "CV_8S",  "CV_8SC1",  "CV_8SC2",  "CV_8SC3",  "CV_8SC4",
+                             "CV_16U", "CV_16UC1", "CV_16UC2", "CV_16UC3", "CV_16UC4",
+                             "CV_16S", "CV_16SC1", "CV_16SC2", "CV_16SC3", "CV_16SC4",
+                             "CV_32S", "CV_32SC1", "CV_32SC2", "CV_32SC3", "CV_32SC4",
+                             "CV_32F", "CV_32FC1", "CV_32FC2", "CV_32FC3", "CV_32FC4",
+                             "CV_64F", "CV_64FC1", "CV_64FC2", "CV_64FC3", "CV_64FC4"};
+
+    for(int i=0; i<numImgTypes; i++)
+    {
+        if(imgTypeInt == enum_ints[i]) return enum_strings[i];
+    }
+    return "unknown image type";
+}
