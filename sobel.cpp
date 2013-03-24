ce4bb7e8ac4e263727af72ee11c6db00ffb7267c
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2013-03-24 14:21:03 +0100

    work in progress

--- sobel.cpp
+++ sobel.cpp
@@ -166,7 +166,83 @@ int main( int argc, char** argv )
   cv::convertScaleAbs(grad_x,grad_x);
   cv::convertScaleAbs(grad_y,grad_y);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  cv::Mat scene(imread( argv[2] ));
+
+  if( !scene.data )
+    { return -1; }
+
+  Mat scene_gray;
+  Mat scene_L1_gradient_magnitude;
+
+  /// Convert it to gray
+  cvtColor( scene, scene_gray, CV_RGB2GRAY );
+  /// Generate grad_x and grad_y
+  Mat scene_grad_x, scene_grad_y;
+  Mat scene_abs_grad_x, scene_abs_grad_y;
+  /// Gradient X
+  Sobel( scene_gray, scene_grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( scene_grad_x, scene_abs_grad_x );
+  /// Gradient Y
+  Sobel( scene_gray, scene_grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( scene_grad_y, scene_abs_grad_y );
+  /// Total Gradient (approximate)
+  addWeighted( scene_abs_grad_x, 1, scene_abs_grad_y, 1, 0, scene_L1_gradient_magnitude );
+
+
+  imshow( "scene L1 gradient magnitude", scene_L1_gradient_magnitude );
+
+  cv::Mat scene_grad_x_f, scene_grad_y_f;
+  scene_grad_x.convertTo(scene_grad_x_f,CV_32F);
+  scene_grad_y.convertTo(scene_grad_y_f,CV_32F);
+
+
+  cv::Mat scene_mag;
+  cv::magnitude(scene_grad_x_f,scene_grad_y_f,scene_mag);
+
+  cv::convertScaleAbs(scene_mag,scene_mag);
+  imshow("scene L2 gradient magnitude",scene_mag);
+
+  cv::Mat scene_mag_bin;
+  cv::threshold(scene_mag,scene_mag_bin,128,255,THRESH_BINARY);
+  imshow("binarized L2 gradient magnitude",scene_mag_bin);
+
+  cv::Mat scene_phi_degree;
+  cv::phase(scene_grad_x_f,scene_grad_y_f,scene_phi_degree,true);
+
+  cv::Mat scene_phi_radian;
+  cv::phase(scene_grad_x_f,scene_grad_y_f,scene_phi_radian,false);
+
+  cv::convertScaleAbs(scene_phi_degree,scene_phi_degree);
+  imshow("scene phase [degree]",scene_phi_degree);  
+
+  cv::Mat scene_phi_on_edge;
+  cv::bitwise_and(scene_phi_degree,scene_mag_bin,scene_phi_on_edge);
+
+  cvtColor( scene_phi_on_edge, scene_phi_on_edge, CV_GRAY2RGB );
+
+  std::vector< cv::Point > scene_mymask;
+  for ( int x = 0; x < scene_phi_on_edge.cols; x+=15 ){
+    for ( int y = 0; y < scene_phi_on_edge.rows; y+=15 ){
+      if(scene_mag_bin.at<unsigned char>(y,x)==255){
+        cv::Point p(x,y);
+        scene_mymask.push_back(p);
+      }
+    }
+  }
+
+  std::cout << "\nSearch:\n";
+  for ( size_t i = 0; i < scene_mymask.size(); i++ ) {
+    float angle = rad2deg(scene_phi_radian.at<float>(scene_mymask[i].y,scene_mymask[i].x));
+    std::pair <R_table_t::iterator, R_table_t::iterator> ret(R_table.equal_range(angle));
+    if (ret.first != ret.second){
+      std::cout << angle << "\n";
+      for (R_table_t::iterator it1=ret.first; it1!=ret.second; ++it1){
+        std::cout << "\t" << it1->second << "\n";
+      }    
+    }
+  }
 
 
 
