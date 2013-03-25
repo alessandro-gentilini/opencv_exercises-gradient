d57ad4e3bef9f8605d4a9de86c4740a98077769b
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2013-03-25 18:42:34 +0100

    deleted using

--- sobel.cpp
+++ sobel.cpp
@@ -9,9 +9,7 @@
 #include <map>
 #include <unordered_map>
 
-using namespace cv;
-
-void draw_cross(cv::Mat& img, const cv::Point center, float arm_length, const Scalar& color )
+void draw_cross(cv::Mat& img, const cv::Point center, float arm_length, const cv::Scalar& color )
 {
   cv::Point N(center-cv::Point(0,arm_length));
   cv::Point S(center+cv::Point(0,arm_length));
@@ -21,7 +19,7 @@ void draw_cross(cv::Mat& img, const cv::Point center, float arm_length, const Sc
   cv::line(img,E,W,color);
 }
 
-void draw_cross_45(cv::Mat& img, const cv::Point center, float arm_length, const Scalar& color )
+void draw_cross_45(cv::Mat& img, const cv::Point center, float arm_length, const cv::Scalar& color )
 {
   cv::Point NE(center+cv::Point(arm_length,arm_length));
   cv::Point SW(center+cv::Point(-arm_length,-arm_length));
@@ -40,17 +38,15 @@ T rad2deg( const T& r )
 struct hash_point {
     size_t operator()(const cv::Point& p ) const
     {
-        std::ostringstream oss;
-        oss << p;
-        return std::hash<std::string>()(oss.str());
+        return std::hash<cv::Point::value_type>()(p.x)^std::hash<cv::Point::value_type>()(p.y);
     }
 };
 
 int main( int argc, char** argv )
 {
 
-  Mat model, model_gray;
-  Mat L1_gradient_magnitude;
+  cv::Mat model, model_gray;
+  cv::Mat L1_gradient_magnitude;
   int scale = 1;
   int delta = 0;
   int ddepth = CV_16S;
@@ -58,38 +54,38 @@ int main( int argc, char** argv )
   int c;
 
   /// Load an image
-  model = imread( argv[1] );
+  model = cv::imread( argv[1] );
 
-  imshow("model",model);
+  cv::imshow("model",model);
 
   if( !model.data )
     { return -1; }
 
-  //GaussianBlur( model, model, Size(3,3), 0, 0, BORDER_DEFAULT );
+  //GaussianBlur( model, model, Size(3,3), 0, 0, cv::BORDER_DEFAULT );
 
   /// Convert it to gray
-  cvtColor( model, model_gray, CV_RGB2GRAY );
+  cv::cvtColor( model, model_gray, CV_RGB2GRAY );
 
   
   /// Generate grad_x and grad_y
-  Mat grad_x, grad_y;
-  Mat abs_grad_x, abs_grad_y;
+  cv::Mat grad_x, grad_y;
+  cv::Mat abs_grad_x, abs_grad_y;
 
   /// Gradient X
-  //Scharr( model_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-  Sobel( model_gray, grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_x, abs_grad_x );
+  //Scharr( model_gray, grad_x, ddepth, 1, 0, scale, delta, cv::BORDER_DEFAULT );
+  Sobel( model_gray, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
+  cv::convertScaleAbs( grad_x, abs_grad_x );
 
   /// Gradient Y
-  //Scharr( model_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-  Sobel( model_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( grad_y, abs_grad_y );
+  //Scharr( model_gray, grad_y, ddepth, 0, 1, scale, delta, cv::BORDER_DEFAULT );
+  Sobel( model_gray, grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT );
+  cv::convertScaleAbs( grad_y, abs_grad_y );
 
   /// Total Gradient (approximate)
   addWeighted( abs_grad_x, 1, abs_grad_y, 1, 0, L1_gradient_magnitude );
 
 
-  imshow( "L1 gradient magnitude", L1_gradient_magnitude );
+  cv::imshow( "L1 gradient magnitude", L1_gradient_magnitude );
 
   
   
@@ -103,11 +99,11 @@ int main( int argc, char** argv )
   cv::magnitude(grad_x_f,grad_y_f,mag);
 
   cv::convertScaleAbs(mag,mag);
-  imshow("L2 gradient magnitude",mag);
+  cv::imshow("L2 gradient magnitude",mag);
 
   cv::Mat mag_bin;
-  cv:threshold(mag,mag_bin,128,255,THRESH_BINARY);
-  imshow("binarized L2 gradient magnitude",mag_bin);
+  cv:threshold(mag,mag_bin,128,255,cv::THRESH_BINARY);
+  cv::imshow("binarized L2 gradient magnitude",mag_bin);
 
   cv::Mat phi_degree;
   cv::phase(grad_x_f,grad_y_f,phi_degree,true);
@@ -116,7 +112,7 @@ int main( int argc, char** argv )
   cv::phase(grad_x_f,grad_y_f,phi_radian,false);
 
   cv::convertScaleAbs(phi_degree,phi_degree);
-  imshow("phase [degree]",phi_degree);  
+  cv::imshow("phase [degree]",phi_degree);  
 
   cv::Mat phi_on_edge;
   cv::bitwise_and(phi_degree,mag_bin,phi_on_edge);
@@ -173,7 +169,7 @@ int main( int argc, char** argv )
       cv::circle(phi_on_edge,tip,2,CV_RGB(0,255,0));    
       std::ostringstream oss;
       oss << i;
-      cv::putText(phi_on_edge,oss.str(),tip,FONT_HERSHEY_SIMPLEX,1,CV_RGB(255,0,0));
+      cv::putText(phi_on_edge,oss.str(),tip,cv::FONT_HERSHEY_SIMPLEX,1,CV_RGB(255,0,0));
     }
   }
   cv::circle(phi_on_edge,centroid,2,CV_RGB(255,255,0)); 
@@ -190,37 +186,37 @@ int main( int argc, char** argv )
 
 
 
-  imshow("phase for pixels belonging to the edge",phi_on_edge);
+  cv::imshow("phase for pixels belonging to the edge",phi_on_edge);
 
   cv::convertScaleAbs(grad_x,grad_x);
   cv::convertScaleAbs(grad_y,grad_y);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  cv::Mat scene(imread( argv[2] ));
+  cv::Mat scene(cv::imread( argv[2] ));
 
   if( !scene.data )
     { return -1; }
 
-  Mat scene_gray;
-  Mat scene_L1_gradient_magnitude;
+  cv::Mat scene_gray;
+  cv::Mat scene_L1_gradient_magnitude;
 
   /// Convert it to gray
-  cvtColor( scene, scene_gray, CV_RGB2GRAY );
+  cv::cvtColor( scene, scene_gray, CV_RGB2GRAY );
   /// Generate grad_x and grad_y
-  Mat scene_grad_x, scene_grad_y;
-  Mat scene_abs_grad_x, scene_abs_grad_y;
+  cv::Mat scene_grad_x, scene_grad_y;
+  cv::Mat scene_abs_grad_x, scene_abs_grad_y;
   /// Gradient X
-  Sobel( scene_gray, scene_grad_x, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( scene_grad_x, scene_abs_grad_x );
+  cv::Sobel( scene_gray, scene_grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
+  cv::convertScaleAbs( scene_grad_x, scene_abs_grad_x );
   /// Gradient Y
-  Sobel( scene_gray, scene_grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-  convertScaleAbs( scene_grad_y, scene_abs_grad_y );
+  cv::Sobel( scene_gray, scene_grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT );
+  cv::convertScaleAbs( scene_grad_y, scene_abs_grad_y );
   /// Total Gradient (approximate)
-  addWeighted( scene_abs_grad_x, 1, scene_abs_grad_y, 1, 0, scene_L1_gradient_magnitude );
+  cv::addWeighted( scene_abs_grad_x, 1, scene_abs_grad_y, 1, 0, scene_L1_gradient_magnitude );
 
 
-  imshow( "scene L1 gradient magnitude", scene_L1_gradient_magnitude );
+  cv::imshow( "scene L1 gradient magnitude", scene_L1_gradient_magnitude );
 
   cv::Mat scene_grad_x_f, scene_grad_y_f;
   scene_grad_x.convertTo(scene_grad_x_f,CV_32F);
@@ -231,11 +227,11 @@ int main( int argc, char** argv )
   cv::magnitude(scene_grad_x_f,scene_grad_y_f,scene_mag);
 
   cv::convertScaleAbs(scene_mag,scene_mag);
-  imshow("scene L2 gradient magnitude",scene_mag);
+  cv::imshow("scene L2 gradient magnitude",scene_mag);
 
   cv::Mat scene_mag_bin;
-  cv::threshold(scene_mag,scene_mag_bin,128,255,THRESH_BINARY);
-  imshow("scene binarized L2 gradient magnitude",scene_mag_bin);
+  cv::threshold(scene_mag,scene_mag_bin,128,255,cv::THRESH_BINARY);
+  cv::imshow("scene binarized L2 gradient magnitude",scene_mag_bin);
 
   cv::Mat scene_phi_degree;
   cv::phase(scene_grad_x_f,scene_grad_y_f,scene_phi_degree,true);
@@ -244,12 +240,12 @@ int main( int argc, char** argv )
   cv::phase(scene_grad_x_f,scene_grad_y_f,scene_phi_radian,false);
 
   cv::convertScaleAbs(scene_phi_degree,scene_phi_degree);
-  imshow("scene phase [degree]",scene_phi_degree);  
+  cv::imshow("scene phase [degree]",scene_phi_degree);  
 
   cv::Mat scene_phi_on_edge;
   cv::bitwise_and(scene_phi_degree,scene_mag_bin,scene_phi_on_edge);
 
-  cvtColor( scene_phi_on_edge, scene_phi_on_edge, CV_GRAY2RGB );
+  cv::cvtColor( scene_phi_on_edge, scene_phi_on_edge, CV_GRAY2RGB );
 
   std::vector< cv::Point > scene_mymask;
   for ( int x = 0; x < scene_phi_on_edge.cols; x+=1 ){
@@ -304,19 +300,19 @@ int main( int argc, char** argv )
   std::cerr << "cnt=" << cnt << " @ " << location << "\n";
 
   cv::Mat acc_to_show;
-  convertScaleAbs( accumulator, acc_to_show );
-  imshow("accumulator",acc_to_show);
+  cv::convertScaleAbs( accumulator, acc_to_show );
+  cv::imshow("accumulator",acc_to_show);
 
   
   
   draw_cross(scene,location,100,CV_RGB(0,0,255));
   draw_cross_45(scene,p_max,100,CV_RGB(255,0,0));
-  imshow("Found",scene);
+  cv::imshow("Found",scene);
 
 
 
 
-  waitKey(0);
+  cv::waitKey(0);
 
   return 0;
 }
